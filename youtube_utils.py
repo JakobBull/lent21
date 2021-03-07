cfa29bb79f5c36956f5d49830dd5ff51df1c5484
nareshr100 <naresh@suriar.me.uk>
2021-03-07 11:28:54 +0000

    neatened qs and added relate qs

--- youtube_utils.py
+++ youtube_utils.py
@@ -1,6 +1,7 @@
 ''' functions to aid with finding video codes from youtube and creating lists of them '''
 
 from youtube_search import YoutubeSearch
+from sql_utils import sqliteExecute
 
 def youtube_search(key_list, max_results=10, return_type='dict'):
 
@@ -35,3 +36,18 @@ def get_video_codes(vid_dict):
         vid_list.append(item['id'])
 
     return vid_list
+
+def get_related_questions(search_key):
+    query = 'SELECT * FROM Questions WHERE topics = ?'
+    params = (search_key,)
+    result = sqliteExecute(query, params)
+
+    related_questions = []
+    max_questions = 3
+    for i, item in enumerate(result):
+        if i < max_questions:
+            related_questions.append((item[0], item[-1]))
+        else:
+            break
+    
+    return related_questions
